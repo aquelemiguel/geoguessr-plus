0f1d4c1cb2d0853b9b049d1a15bc5fb968cfb171
Miguel Fernandes <miguelmanofernandes@gmail.com>
2021-04-09 02:53:30 +0100

    Only allow up to 8 previous flags to be displayed on streak games

--- scripts/streak-show-flags.js
+++ scripts/streak-show-flags.js
@@ -3,6 +3,14 @@
     const response = await fetch(`https://www.geoguessr.com/api/v3/games/${game_id}`);
     const game = await response.json();
 
+    /**
+     * Ensure this isn't running on US state streak mode.
+     * Support coming soon!
+     */
+    if (game.mode === 'standard' || game.map === 'us-state-streak') {
+        return;
+    }
+
     const canvas_elem = document.querySelector('div.game-layout__canvas');
     
     function create_flag(country_code) {
@@ -18,28 +26,48 @@
         return flag_elem.firstChild;
     }
 
-    /**
-     * Ensure this isn't running on US state streak mode.
-     * Support coming soon!
-     */
-    if (game.map === 'us-state-streak') {
-        return;
+    async function inject_flags() {
+        const response = await fetch(`https://www.geoguessr.com/api/v3/games/${game_id}`);
+        const game = await response.json();
+
+        /**
+         * Don't inject the component if the player hasn't guessed yet.
+         */
+        if (game.player.guesses.length === 0) {
+            return;
+        }
+
+        /**
+         * If the element is already there, delete it and reinject it.
+         */
+        document.querySelector('div.flag-container')?.remove();
+
+        let flag_elems = game.player.guesses.map(guess => create_flag(guess.streakLocationCode).outerHTML);
+        flag_elems = flag_elems.reverse().slice(Math.min(flag_elems.length - 2, 7));
+
+        const flag_container = document.createElement('div');
+        flag_container.innerHTML = `
+            <div class="game-layout__status flag-container" 
+                style="display: flex; position: absolute; z-index: 1; left: 2rem;">
+                <div class="game-statuses">
+                    <div class="game-status" style="padding: 0; border: none;">
+                        <div class="game-status__heading">Previous guesses</div>
+                    </div>
+                    <div class="flags" style="display: flex;">
+                        ${flag_elems.join('')}
+                    </div>
+                </div>
+            </div>
+        `.trim();
+
+        canvas_elem.appendChild(flag_container.firstChild);
     }
 
     /**
      * Listen for game score change and request the game state.
      */
-    let observer = new MutationObserver(async mutations => {
-        const no_flags_yet_elem = document.querySelector('div#no-flags-yet');
-        if (no_flags_yet_elem) { no_flags_yet_elem.remove() };
-
-        const response = await fetch(`https://www.geoguessr.com/api/v3/games/${game_id}`);
-        const game = await response.json();
-
-        const flags_container = document.querySelector('div.game-statuses > div.flags');
-        const guessed_country = game.player.guesses[parseInt(mutations[0].target.textContent - 1)].streakLocationCode;
-
-        flags_container.appendChild(create_flag(guessed_country));
+    let observer = new MutationObserver(async _mutations => {
+        inject_flags();
     });
 
     observer.observe(document.querySelector('div.game-statuses > div:first-child'), {
@@ -47,24 +75,5 @@
         characterData: true,
     });
 
-    const flag_elems = game.player.guesses.map(guess => create_flag(guess.streakLocationCode).outerHTML);
-
-    const flag_container = document.createElement('div');
-    flag_container.innerHTML = `
-        <div class="game-layout__status" 
-            style="display: flex; position: absolute; z-index: 1; left: 2rem;">
-            <div class="game-statuses">
-                <div class="game-status" style="padding: 0; border: none;">
-                    <div class="game-status__heading">Previous guesses</div>
-                </div>
-                <div class="flags" style="display: flex;">
-                    ${flag_elems.length > 0 ? 
-                        flag_elems.join('') : 
-                        '<div class="game-status__body" id="no-flags-yet" style="flex: 1; text-align: center;">N/A</div>'}
-                </div>
-            </div>
-        </div>
-    `.trim();
-
-    canvas_elem.appendChild(flag_container.firstChild);
+    inject_flags();
 })();
